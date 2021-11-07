24d2451c1751ef56af042ad5de1bfb09d794a3de
xCaiudo <69972004+xCaiudo@users.noreply.github.com>
2021-11-07 16:39:54 -0300

    Add files via upload

---
+++ src/main/java/br/com/letscode/spring/pdv/PdvApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.spring.pdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdvApplication.class, args);
+    }
+
+}
