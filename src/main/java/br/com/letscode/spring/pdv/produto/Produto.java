dc3449920e525f18b3e2fcab538e1930be091a21
Arthur <arthurgarcia_08@outlook.com>
2021-11-08 19:16:51 -0300

    Adicionando produto

--- src/main/java/br/com/letscode/spring/pdv/produto/Produto.java
+++ src/main/java/br/com/letscode/spring/pdv/produto/Produto.java
@@ -1,4 +1,27 @@
 package br.com.letscode.spring.pdv.produto;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    private Integer valor;
+    private Integer qtd;
+    @Column(unique=true)
+    private Integer cod_interno;
+
 }
