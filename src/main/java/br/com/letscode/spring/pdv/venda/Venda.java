d5eaaac8cb7d8f7b6488129039d03069dbec0c6d
Arthur <arthurgarcia_08@outlook.com>
2021-11-09 17:19:52 -0300

    Adicionando venda

--- src/main/java/br/com/letscode/spring/pdv/venda/Venda.java
+++ src/main/java/br/com/letscode/spring/pdv/venda/Venda.java
@@ -1,4 +1,38 @@
 package br.com.letscode.spring.pdv.venda;
 
+import br.com.letscode.spring.pdv.cliente.Cliente;
+import br.com.letscode.spring.pdv.funcionario.Funcionario;
+import lombok.*;
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
 public class Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+
+    @Column(nullable = false)
+    private String id_funcionario;
+    private String id_cliente;
+    private Integer total;
+    private Integer data_venda;
+
+    @ManyToOne
+    @JoinColumn(name = "id_funcionario")
+    private Funcionario funcionario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+
+
 }
