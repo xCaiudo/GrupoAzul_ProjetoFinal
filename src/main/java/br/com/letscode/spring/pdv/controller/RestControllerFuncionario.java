54f694765d68269f620ab4e47b0477dc7f7558d4
ariane.brito <ariane.brito@faculdadeimpacta.com.br>
2021-11-09 17:13:58 -0300

    Classe funcionario iniciada

---
+++ src/main/java/br/com/letscode/spring/pdv/controller/RestControllerFuncionario.java
@@ -0,0 +1,7 @@
+package br.com.letscode.spring.pdv.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerFuncionario {
+}
--- src/main/java/br/com/letscode/spring/pdv/funcionario/Funcionario.java
+++ src/main/java/br/com/letscode/spring/pdv/funcionario/Funcionario.java
@@ -1,4 +1,21 @@
 package br.com.letscode.spring.pdv.funcionario;
 
+import lombok.*;
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
 public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private int cod_interno;
+    private String email;
+    private String telefone;
 }
+
