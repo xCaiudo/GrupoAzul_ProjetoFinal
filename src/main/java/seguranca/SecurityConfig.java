6f429548dd641c6e29782472bdd1e32d97d4e460
ArthurVillela <arthurvillela@hotmail.com>
2021-11-11 18:30:51 -0300

    Feito Login Inicial, liberado pra qualquer "GET"

---
+++ src/main/java/seguranca/SecurityConfig.java
@@ -0,0 +1,22 @@
+package seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers(HttpMethod.GET, "/**").permitAll() // <<< LIBERAR TUDO apenas para HTTP GET
+                .and()
+                .formLogin().and()
+                .httpBasic();
+    }
+}
